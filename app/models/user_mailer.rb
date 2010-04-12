f758c291b8c96e409e3dbcefa65094f6d9efc4e9
Maulin Pathare <mPathare@gmail.com>
2010-04-11 20:13:39 -0400

    chenged from address for emails in usermailer to admin@sl.maulinpathare.com

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,42 +1,42 @@
 class UserMailer < ActionMailer::Base
   def registration_confirmation(player, password)
     recipients		player.email
-    from		"donotreply@sportsladder.com"       
+    from		"admin@sl.maulinpathare.com"       
     subject		"Sports Ladder registration"
     body		:player => player, :password => password
   end
 
   def challenger_challenge_issued(challenger, defender, ladder)
     recipients		challenger.email
-    from		"donotreply@sportsladder.com"       
+    from		"admin@sl.maulinpathare.com"       
     subject		"Challenge notification"
     body		:challenger => challenger, :defender => defender, :ladder => ladder
   end
 
   def defender_challenge_issued(challenger, defender, ladder)
     recipients		defender.email
-    from		"donotreply@sportsladder.com"       
+    from		"admin@sl.maulinpathare.com"       
     subject		"Challenge notification"
     body		:challenger => challenger, :defender => defender, :ladder => ladder
   end
 
   def cancel_challenge(challenger, defender, ladder)
     recipients		[defender.email, challenger.email]
-    from		"donotreply@sportsladder.com"       
+    from		"admin@sl.maulinpathare.com"       
     subject		"Challenge cancelled"
     body		:challenger => challenger, :defender => defender, :ladder => ladder
   end
 
   def reset_password(player)
     recipients		player.email
-    from		"donotreply@sportsladder.com"       
+    from		"admin@sl.maulinpathare.com"       
     subject		"Password reset request"
     body		:player => player
   end
 
   def new_comment(commenter, player, ladder, comment)
     recipients		player.email
-    from		"donotreply@sportsladder.com"       
+    from		"admin@sl.maulinpathare.com"       
     subject		"New comment"
     body		:player => player, :comment => comment, :ladder => ladder, :commenter => commenter
   end
